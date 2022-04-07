a5b075d590b2e6b90565d5cc3622ebf2b794ac72
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-07 12:34:00 +0200

    added firebase file and register page form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const handleSubmit = () => {};
+
+  //this form will send a request to firebase
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="email" 
+      className="form-control" 
+      value={email} 
+      onChange={e => 
+      setEmail(e.target.value)}
+      autoFocus
+       />
+       <button type="submit" className="btn btn-raised">Register</button>
+    </form>
+  );
+
   return (
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
-          
+          {registerForm()}
         </div>
       </div>
-      
     </div>
   );
 };
