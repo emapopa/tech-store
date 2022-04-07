df58ab06a6673fb6f91ba9e629a05e1034206a12
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-07 13:22:02 +0200

    added register functionality that send link to email via firebse

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import  {auth}  from "../../firebase.js";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const Register = () => {
   const [email, setEmail] = useState("");
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+      // console.log("ENV --->", process.env.REACT_APP_REGISTER_REDIRECT_URL);
+    const config = {
+      url: 'http://localhost:3000/register/complete',
+      handleCodeInApp: true
+    }
+  
+
+  await auth.sendSignInLinkToEmail(email, config);
+    toast.success(
+      `Email is sent to ${email}. Click the link to complete your registration.`
+    );
+    // save user email in local storage
+    window.localStorage.setItem("emailForRegistration", email);
+    // clear state
+    setEmail("");
+  };
 
   //this form will send a request to firebase
+  //and send to the user a link with wich he could register
   const registerForm = () => (
     <form onSubmit={handleSubmit}>
       <input 
@@ -24,6 +46,7 @@ const Register = () => {
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
+          <ToastContainer />
           {registerForm()}
         </div>
       </div>
