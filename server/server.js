97ba54051a364fd38a35461dbd2239a619e18589
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-09 11:15:49 +0200

    added user/routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const morgan = require("morgan"); // to see in the terminal the http requrst
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
 
-// import routes
-const authRoutes = require("./routes/auth");
+
 
 // app
 const app = express();
@@ -23,7 +23,7 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
 // route  middleware
-app.use("/api", authRoutes);
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
