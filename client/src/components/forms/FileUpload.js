272ba0a9fdafad6d9e8e94a7a5e58b2d44412dcd
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-13 15:26:06 +0200

    added multiple image uppload

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -3,7 +3,7 @@ import Resizer from "react-image-file-resizer";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-const FileUpload = () => {
+const FileUpload = ({ values, setValues, setLoading }) => {
   const { user } = useSelector((state) => ({ ...state }));
 
   const fileUploadAndResize = (e) => {
@@ -11,7 +11,9 @@ const FileUpload = () => {
     // console.log(e.target.files);
     // resize
     let files = e.target.files; // 3
+    let allUploadedFiles = values.images;
     if (files) {
+      setLoading(true);
       for (let i = 0; i < files.length; i++) {
         Resizer.imageFileResizer(
           files[i],
@@ -21,7 +23,28 @@ const FileUpload = () => {
           100,
           0,
           (uri) => {
-            console.log(uri);
+           // console.log(uri);
+           axios
+           .post(
+             `${process.env.REACT_APP_API}/uploadimages`,
+             { image: uri },
+             {
+               headers: {
+                 authtoken: user ? user.token : "",
+               },
+             }
+           )
+           .then((res) => {
+             console.log("IMAGE UPLOAD RES DATA", res);
+             setLoading(false);
+             allUploadedFiles.push(res.data);
+
+             setValues({ ...values, images: allUploadedFiles });
+           })
+           .catch((err) => {
+             setLoading(false);
+             console.log("CLOUDINARY UPLOAD ERR", err);
+           });
           },
           "base64"
         );
