aaa55719831368cbfd0dce0fd954a4e3de04f1a6
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-13 10:29:43 +0200

    addded create product

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -22,6 +22,9 @@ const initialState = {
 };
 const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
+  
+  // redux
+  const { user } = useSelector((state) => ({ ...state }));
 
   // destructure
   const {
@@ -42,11 +45,19 @@ const ProductCreate = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //
+    createProduct(values, user.token)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.response.status === 400) toast.error(err.response.data);
+    });
   };
 
   const handleChange = (e) => {
-    //
+    setValues({ ...values, [e.target.name]: e.target.value });
+    // console.log(e.target.name, " ----- ", e.target.value);
   };
   return (
     <div className="container-fluid">
