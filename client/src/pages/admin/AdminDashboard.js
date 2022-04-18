9642454482da48cb1309f24c803f09cfa753ef36
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-18 10:24:16 +0200

    added show product card component

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import AdminNav from "../../components/nav/AdminNav";
 import { getProductsByCount } from "../../functions/product";
+import AdminProductCard from "../../components/cards/AdminProductCard";
 
 
 const AdminDashboard = () => {
@@ -29,13 +30,21 @@ const AdminDashboard = () => {
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
+        </div >
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <AdminProductCard product={product} />
+              </div>
+            ))}
+          </div>
         </div>
-        {loading ? (
-          <h4 className="text-danger">Loading...</h4>
-        ) : (
-          <h4>All Products</h4>
-        )}
-        <div className="col">{JSON.stringify(products)}</div>
       </div>
     </div>
   );
