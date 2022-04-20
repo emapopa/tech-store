04ca1ebe0a0588b6deb3a4a3e7132fd6d380605f
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-20 08:47:22 +0200

    added new arrival pagination

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -27,10 +27,12 @@ export const createProduct = async (product, authtoken) =>
     },
   });
 
-  export const getProducts = async (sort, order, limit) =>
+  export const getProducts = async (sort, order, page) =>
   await axios.post(`${process.env.REACT_APP_API}/products`, {
     sort,
     order,
-    limit,
+    page,
   });
 
+  export const getProductsCount = async () =>
+  await axios.get(`${process.env.REACT_APP_API}/products/total`);
