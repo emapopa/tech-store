9965e02d75997d665a11d3aec636e2619caf3205
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-22 07:32:47 +0200

    added raitnign start modal

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -7,6 +7,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
+import RatingModal from "../modal/RatingModal";
 
 const { TabPane } = Tabs;
 
@@ -36,17 +37,8 @@ const SingleProduct = ({ product }) => {
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
-
-        <StarRating
-          name={_id}
-          numberOfStars={5}
-          rating={2}
-          changeRating={(newRating, name) =>
-            console.log("newRating", newRating, "name", name)
-          }
-          isSelectable={true}
-          starRatedColor="red"
-        />
+       
+      
         <Card
           actions={[
             <>
@@ -56,6 +48,18 @@ const SingleProduct = ({ product }) => {
             <Link to="/">
               <HeartOutlined className="text-info" /> <br /> Add to Wishlist
             </Link>,
+             <RatingModal>
+             <StarRating
+               name={_id}
+               numberOfStars={5}
+               rating={2}
+               changeRating={(newRating, name) =>
+                 console.log("newRating", newRating, "name", name)
+               }
+               isSelectable={true}
+               starRatedColor="red"
+             />
+           </RatingModal>
           ]}
         >
           <ProductListItems product={product} />
