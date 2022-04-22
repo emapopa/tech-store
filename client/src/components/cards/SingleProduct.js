807d9e81a13c35652e9bbf234a85e5031519f920
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-22 08:54:47 +0200

    showing avarge raiting on product card page

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -8,6 +8,7 @@ import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
 import RatingModal from "../modal/RatingModal";
+import { showAverage } from "../../functions/rating"; 
 
 const { TabPane } = Tabs;
 // this is childrend component of Product page
@@ -37,6 +38,11 @@ const SingleProduct = ({ product, onStarClick, star  }) => {
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
+
+        {product && product.ratings && product.ratings.length > 0
+          ? showAverage(product)
+          : "No rating yet"}
+
        
       
         <Card
