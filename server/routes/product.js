970a058b0024c35ed9941f3afc1b83c3b3e024f8
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-22 07:16:53 +0200

    added raiting product  in backend

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-const { create, listAll, remove, read,  update, list,  productsCount,} = require("../controllers/product");
+const { create, listAll, remove, read,  update, list,  productsCount, productStar,} = require("../controllers/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
@@ -17,5 +17,6 @@ router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
 
 router.post("/products", list);
-
+// rating
+router.put("/product/star/:productId", authCheck, productStar);
 module.exports = router;
