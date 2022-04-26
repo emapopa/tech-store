85f07da02868d690277f77a97025e94d71e8ac76
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-26 14:20:13 +0200

    added toltip on product card components

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Tooltip } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
@@ -8,6 +8,9 @@ import _ from "lodash";
 
 const { Meta } = Card;
 const ProductCard = ({ product }) => {
+  const [tooltip, setTooltip] = useState("Click to add");
+
+
   const handleAddToCart = () => {
   let cart = [];
     if (typeof window !== "undefined") {
@@ -25,6 +28,8 @@ const ProductCard = ({ product }) => {
       // save to local storage
       // console.log('unique', unique)
       localStorage.setItem("cart", JSON.stringify(unique));
+       // show tooltip
+       setTooltip("Added");
     }
   };
      // destructure
@@ -50,9 +55,12 @@ const ProductCard = ({ product }) => {
           <EyeOutlined className="text-warning" /> <br /> View Product
         </Link>,
     
-        <a onClick={handleAddToCart}>
-            <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-          </a>,
+    <Tooltip title={tooltip}>
+    <a onClick={handleAddToCart}>
+      <ShoppingCartOutlined className="text-danger" /> <br /> Add to
+      Cart
+    </a>
+  </Tooltip>,
       ]}
     >
       <Meta
