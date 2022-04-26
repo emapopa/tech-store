fe6e655ca31c439dc2e298dd3dcffd6f03ed20f3
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-04-26 10:12:00 +0200

    added category filter on product controllers

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -164,6 +164,8 @@ exports.listRelated = async (req, res) => {
     .exec();
 
   res.json(related);
+
+  
 };
 // SERACH / FILTER
 
@@ -196,6 +198,20 @@ const handlePrice = async (req, res, price) => {
   }
 };
 
+const handleCategory = async (req, res, category) => {
+  try {
+    let products = await Product.find({ category })
+      .populate("category", "_id name")
+      .populate("subs", "_id name")
+      .populate("postedBy", "_id name")
+      .exec();
+
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 
 exports.searchFilters = async (req, res) => {
   const { query, price } = req.body;
@@ -210,4 +226,9 @@ exports.searchFilters = async (req, res) => {
     console.log("price ---> ", price);
     await handlePrice(req, res, price);
   }
+  if (category) {
+    console.log("category ---> ", category);
+    await handleCategory(req, res, category);
+  }
+
 };
