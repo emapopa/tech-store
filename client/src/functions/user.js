b248c52d8b3c832851e4ed6dd549eaf3b3aeb517
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-05-02 11:56:34 +0200

    added empty cart

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -17,3 +17,10 @@ export const userCart = async (cart, authtoken) =>
       authtoken,
     },
   });
+
+  export const emptyUserCart = async (authtoken) =>
+  await axios.delete(`${process.env.REACT_APP_API}/user/cart`, {
+    headers: {
+      authtoken,
+    },
+  });
