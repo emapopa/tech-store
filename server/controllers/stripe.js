9dda5b5a43ef78abd094c79e3d4a0ff3b465ba13
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-05-02 15:42:26 +0200

    added charging actual amount

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -8,11 +8,17 @@ exports.createPaymentIntent = async (req, res) => {
   // later apply coupon
   // later calculate price
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
-    currency: "sek",
-  });
+ // 1 find user
+ const user = await User.findOne({ email: req.user.email }).exec();
+ // 2 get user cart total
+ const { cartTotal } = await Cart.findOne({ orderdBy: user._id }).exec();
 
+ console.log("CART TOTAL CHARGED", cartTotal);
+ // create payment intent with order amount and currency
+ const paymentIntent = await stripe.paymentIntents.create({
+   amount: cartTotal * 100,
+   currency: "sek",
+ });
   res.send({
     clientSecret: paymentIntent.client_secret,
   });
