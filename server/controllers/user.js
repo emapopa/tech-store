ba9a01c02aa10c715cfa805d4fb43cd885bd43d6
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2022-05-02 13:09:06 +0200

    added apply discount backend

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -79,3 +79,38 @@ exports.saveAddress = async (req, res) => {
 
   res.json({ ok: true });
 };
+
+exports.applyCouponToUserCart = async (req, res) => {
+  const { coupon } = req.body;
+  console.log("COUPON", coupon);
+
+  const validCoupon = await Coupon.findOne({ name: coupon }).exec();
+  if (validCoupon === null) {
+    return res.json({
+      err: "Invalid coupon",
+    });
+  }
+  console.log("VALID COUPON", validCoupon);
+
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let { products, cartTotal } = await Cart.findOne({ orderdBy: user._id })
+    .populat("products.product", "_id title price")
+    .exec();
+
+  console.log("cartTotal", cartTotal, "discount%", validCoupon.discount);
+
+  // calculate the total after discount
+  let totalAfterDiscount = (
+    cartTotal -
+    (cartTotal * validCoupon.discount) / 100
+  ).toFixed(2); // 99.99
+
+  Cart.findOneAndUpdate(
+    { orderdBy: user._id },
+    { totalAfterDiscount },
+    { new: true }
+  );
+
+  res.json(totalAfterDiscount);
+};
